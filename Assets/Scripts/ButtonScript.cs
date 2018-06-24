f1aae7b6e9f43045dd8739a1dbac35f7fa42d6db
Unknown <adh416@nyu.edu>
2018-06-24 01:36:53 -0400

    Background Changing Script

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+    Animator buttonAnim;
+    public bool animEnd;
+
+    private void Update()
+    {
+        if (animEnd)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Play button animation
+        buttonAnim = GetComponent<Animator>();
+        if(buttonAnim != null)
+        {
+            Debug.Log("Playing anim");
+            //buttonAnim.Play("");
+        }
+        
+        // Play sound
+        // Play TV animation
+        // Camera movment? and bool change
+        // Transition scene after a certain amount of time
+    }
+}
